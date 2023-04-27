251d0c8bff3f1be74e12af7ff0e47ca76f7784dc
Soar1826 <34289582+Soar1826@users.noreply.github.com>
2023-04-27 11:38:37 -0700

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import icon from "../../public/assets/background/laskyline.svg";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      </Head>
       <body>
         <Main />
         <NextScript />

e9967830601f6e74f214799254498c63d59b2f37
Soar1826 <34289582+Soar1826@users.noreply.github.com>
2023-04-27 12:10:43 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -142,7 +142,7 @@ export default function Home() {
             </p>
             <p className="mt-4">
               It doesn&apos;t matter whether you&apos;re a seasoned veteran, or
-              a complete beginner, we&apos;ll love for you to come and hang out!
+              a complete beginner, we&apos;d love for you to come and hang out!
             </p>
             <p className={nunito.className + " mt-4"}>
               AngelHacks LA is a Game Jam organized by people just like you!
